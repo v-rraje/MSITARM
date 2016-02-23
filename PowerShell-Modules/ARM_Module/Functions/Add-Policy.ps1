a75d55ceac9929a83238f136f820b805c0927c5f
Matthew Raffaele <MATTRAFF@microsoft.com>
2016-02-23 15:02:15 -0800

    Updated psm1 to dynamically load functions and scripts

---
+++ PowerShell-Modules/ARM_Module/Functions/Add-Policy.ps1
@@ -0,0 +1,193 @@
+﻿function Add-Policy
+{
+    #this allows for cmdlet-style parameter binding capabilities
+    [CmdletBinding()]
+    Param( 
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('SDOStdPolicyTags','SDOStdPolicyNetwork','SDOStdPolicyRegion')] 
+        [String] 
+        $policy,
+        
+        [Parameter(Mandatory=$true)]
+        [string]$subscriptionID
+     )
+
+    #string manipulation
+    $scope = '/subscriptions/{0}/' -f $subscriptionID
+    Write-Verbose -Message "SubscriptionID is $subscriptionID"
+
+    #string manipulation
+    $policy = $policy.ToLower()
+    
+    #Create policy and assign it at the subscription level
+    switch ($policy) 
+    { 
+        'sdostdpolicytags' 
+        {
+            try
+            {
+                #Set Policy for SDOStdPolicyTags
+                $polDef = New-AzureRmPolicyDefinition -Name SDOStdPolicyTags -Description 'Mandatory tags for billing and ServiceNow integration' -Policy '{
+                  "if": {
+                    "not": {
+                      "AnyOf": [
+                        {
+                          "AllOf": [
+                            {
+                              "field": "tags",
+                              "containsKey": "appID"
+                            },
+                            {
+                              "field": "tags",
+                              "containsKey": "env"
+                            },
+                            {
+                              "field": "tags",
+                              "containsKey": "orgID"
+                            }
+                          ]
+                        },
+                        {
+                          "AllOf": [
+                            {
+                              "field": "tags",
+                              "containsKey": "orgID"
+                            },
+                            {
+                              "field": "tags",
+                              "containsKey": "AppTechComponentID"
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  },
+                  "then": {
+                    "effect": "deny"
+                  }
+                }'
+                
+          
+                #apply at subscription level
+                New-AzureRmPolicyAssignment -Name SDOStdPolicyTags -PolicyDefinition $polDef -Scope $scope | Out-Null
+          
+                Write-Verbose 'SDOStdPolicyTags policy created successfully'
+            }
+            catch 
+            {
+                Write-Verbose 'SDOStdPolicyTags policy creation failed'
+                exit
+            }
+        } 
+        'sdostdpolicynetwork' 
+        {
+            try
+            {
+                #Set Policy for SDOStdPolicyNetwork
+                $polDef = New-AzureRmPolicyDefinition -Name SDOStdPolicyNetwork -Description 'No endpoints, no V1 resources, and some network resources' -Policy '{
+                    "if": {
+                    "anyOf": [
+                    {
+                    "source": "action",
+                    "like": "Microsoft.Network/publicIPAddresses/*"
+                    },
+                    {
+                    "source": "action",
+                    "like": "Microsoft.Network/routeTables/*"
+                    },
+                    {
+                    "source": "action",
+                    "like": "Microsoft.Network/networkSecurityGroups/*"
+                    },
+                    {
+                    "source": "action",
+                    "like": "Microsoft.ClassicCompute/*"
+                    },
+                    {
+                    "source": "action",
+                    "like": "Microsoft.ClassicStorage/*"
+                    },
+                    {
+                    "source": "action",
+                    "like": "Microsoft.ClassicNetwork/*"
+                    }
+                    ]
+                    },
+                    "then": {
+                    "effect": "deny"
+                    }
+                }'
+                
+                         
+                #apply at subscription level
+                New-AzureRmPolicyAssignment -Name SDOStdPolicyNetwork -PolicyDefinition $polDef -Scope $scope | Out-Null
+          
+                Write-Verbose 'SDOStdPolicyNetwork policy created successfully'
+            }
+            catch 
+            {
+                Write-Verbose 'SDOStdPolicyTags policy creation failed'
+                exit
+            }
+
+        } 
+        'sdostdpolicyregion' 
+        {
+            try
+            {
+                #Set Policy for SDOStdPolicyRegion
+                $polDef = New-AzureRmPolicyDefinition -Name SDOStdPolicyRegion -Description 'All resources in Central US' -Policy '{
+                  "if": {
+                    "not": {
+                      "field": "location",
+                      "in": [ "centralus" ]
+                    }
+                  },
+                  "then": {
+                    "effect": "deny"
+                  }
+                }'
+                
+
+          
+                #apply at subscription level
+                New-AzureRmPolicyAssignment -Name SDOStdPolicyRegion -PolicyDefinition $polDef -Scope $scope | Out-Null
+          
+                Write-Verbose 'SDOStdPolicyRegions policy created successfully'
+            }
+            catch 
+            {
+                Write-Verbose 'SDOStdPolicyRegions policy policy creation failed'
+                exit
+            }
+        } 
+        default 
+        {
+            write-output 'Should never get here, switch statement didnt match a case'
+        }
+    }
+
+    return $true
+
+    <#
+            .SYNOPSIS
+            Creates and applies standard SDO policies at the subscription level
+            .DESCRIPTION
+            This is broken by 3 functional areas: network, tags, and 
+            .INPUTS
+            Policy from this set: 'SDOStdPolicyTags','SDOStdPolicyNetwork','SDOStdPolicyRegion' 
+            .OUTPUTS
+            $true or $false
+            -Verbose gives step by step output
+            .EXAMPLE
+            $subID = e8a32032-cc6c-4x56-b451-f07x3fdx47xx 		  		 
+            Add-Policy -policy SDOStdPolicyNetwork -subscriptionID  $subID -Verbose
+            .EXAMPLE 
+            $subID = e8a32032-cc6c-4x56-b451-f07x3fdx47xx 	
+            Add-Policy -policy SDOStdPolicyTags -subscriptionID  $subID -Verbose
+            .EXAMPLE 
+            $subID = e8a32032-cc6c-4x56-b451-f07x3fdx47xx 
+            Add-Policy -policy SDOStdPolicyRegion -subscriptionID  $subID -Verbose
+
+    #>	
+}
--- PowerShell-Modules/ARM_Module/sample.ps1
+++ PowerShell-Modules/ARM_Module/sample.ps1
@@ -1,28 +1,24 @@
 ﻿
-#Authenticate against Azure and set context to your subscription
+# Authenticate against Azure and set context to your subscription
 $subID ='e4xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
 
 Login-AzureRmAccount -SubscriptionId $subID
 
 Set-AzureRmContext -SubscriptionId $subID
 
-#Import Module
-Import-Module .\CPTARM -Force 
+# Import Module
+Import-Module .\CPTARM.psm1 -Force 
 
-#Sample commands
+# Sample commands
 Get-Help Set-DevOpsPermissions -Full
 
+# Add a user by UPN
 Set-DevOpsPermissions -subscriptionID $subID -appRG ResourceGroupName -ERRG ARMERVNETUSCPOC -DevOpsUpn 'user@microsoft.com' -Verbose
 
+# Add multiple users by SG membership
 Set-DevOpsPermissions -subscriptionID $subID -appRG ResourceGroupName -ERRG ARMERVNETUSCPOC -DevOpsGroupName 'SG Display Name' -Verbose
 
-#Discover our commandlets
-#get-command -Module CPTARM
 
-#get details just like any other commandlet
-#help add-policy -Full
-
-#help Add-SDOManagedExpressRouteUserRole -Full
 
 
 
