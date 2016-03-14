e365ea17abf660680f84b3fb052ab8f85b9e0ff8
Tracy Worthington <trworth@microsoft.com>
2016-03-14 02:37:11 -0700

    updates

--- ARMPilot/DeploymentDemo.ps1
+++ ARMPilot/DeploymentDemo.ps1
@@ -1,33 +1,38 @@
 #
 # DeploymentDemo.ps1
 #
-# Scenario - Build 1 IIS and 1 SQL server
+# Scenario - Build 1 by Name
+# Scenario - Build 1 IIS and 1 SQL server by namepar
 #
-import-module cloudms
+#import-module cloudms
 
-show-HelpOnImage
+#Get domain credentials that need to be used for domain joining the VMs
+$username= Read-Host -Prompt "Domain UserName (domainname\alias)"
+$password =Read-Host -Prompt "Password for $username" -AsSecureString
+$domainUserCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
+
+
 
 # Image IIS
 $params = @{
-                   "TemplateFile"="template.json"; 
-                   "TemplateParameterFile"="templateIISParams.json"; 
+                   "TemplateFile"="C:\Users\trworth\Source\Repos\SI-HDC-CPT-ARM\ARMPilot\template-SingleVM.json"; 
+                   "TemplateParameterFile"="C:\Users\trworth\Source\Repos\SI-HDC-CPT-ARM\ARMPilot\templateParams.json"; 
                    "SubscriptionId"="e4a74065-cc6c-4f56-b451-f07a3fde61de"; 
                    "ResourceGroupLocation"="central us"; 
                    "ResourceGroupName"="cptApp1";
                    
                   }
-
+                  
 #Enter your name and specifications for the IIS server.
-Invoke-ARMtemplate -TemplateFile $params.TemplateFile `
+Invoke-ARM -TemplateFile $params.TemplateFile `
        -TemplateParameterFile $params.TemplateParameterFile `
        -SubscriptionId $params.SubscriptionId `
        -ResourceGroupLocation $params.ResourceGroupLocation `
        -ResourceGroupName $params.ResourceGroupName `
-       -PromptToContinue
-       
-# Image SQL
+       -Vm "MyArmTestVM"
 
-reset-cache
+      
+# Image SQL
 
 $params = @{
                    "TemplateFile"="templateSQL.json"; 
@@ -39,20 +44,24 @@ $params = @{
                   }
 
 #Enter your name and specifications for the IIS server.
-Invoke-ARMtemplate -TemplateFile $params.TemplateFile `
+Invoke-ARM -TemplateFile $params.TemplateFile `
        -TemplateParameterFile $params.TemplateParameterFile `
        -SubscriptionId $params.SubscriptionId `
        -ResourceGroupLocation $params.ResourceGroupLocation `
-       -ResourceGroupName $params.ResourceGroupName `
-       -PromptToContinue
+       -ResourceGroupName $params.ResourceGroupName 
+
+Install-VMDomainJoin -resourceGroupName $params.ResourceGroupName  -DomainCredential $domainUserCredential -Domain $domain
 
 # Build IIS
+
+Setup-IIS -Servers @{"Servername"="ArmPilot-SQL-0"} -outputPath $PSScriptRoot\InstallIIS_DSC -ResourceGroupName $params.ResourceGroupName  -DomainCredential  $domainUserCredential
 
 $mof=InstallIIS_DSC -outputPath $PSScriptRoot\InstallIIS_DSC -MachineName ArmPilot-IIS-0
 Get-ChildItem -Path $mof.Directoryname | ConvertTo-MrMOFv4 -Verbose
 Start-DscConfiguration $PSScriptRoot\InstallIIS_DSC -ComputerName ArmPilot-IIS-0   -Verbose -Wait
 
 # Build SQL
+Setup-SQL -Servers @{"Servername"="ArmPilot-SQL-0"} -outputPath $PSScriptRoot\InstallSQL_DSC -ResourceGroupName $params.ResourceGroupName  -DomainCredential  $domainUserCredential
 
 $mof=InstallSQL_DSC -outputPath $PSScriptRoot\InstallSQL_DSC -MachineName ArmPilot-SQL-0
 Get-ChildItem -Path $mof.Directoryname | ConvertTo-MrMOFv4 -Verbose
---
+++ ARMPilot/ERDomainJoin.ps1
@@ -0,0 +1,34 @@
+﻿Param(
+         [string] [Parameter(Mandatory=$true)] $SubscriptionId,
+         [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+         [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+         [string] $TemplateFile,
+         [string] $TemplateParameterFile,
+         [string] $domain="redmond.corp.microsoft.com",
+         [string] [Parameter(Mandatory=$true)] $vm,
+         [switch] $NoImage
+        
+       )
+
+
+if(!$noImage) {
+    #Image the VM using the resource group through ARM template
+    #Get-AzureConnection is called within Invoke-ARM
+
+    Write-Host (Get-Date).ToString()
+    Invoke-Arm -$SubscriptionId $subscriptionId -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile -Verbose -vmName $vm
+    Write-Host (Get-Date).ToString()
+
+}
+
+
+
+#Get domain credentials that need to be used for domain joining the VMs
+$username= Read-Host -Prompt "Domain UserName (domainname\alias)"
+$password =Read-Host -Prompt "Password for $username" -AsSecureString
+$domainUserCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
+
+#Iterate through all the RM VMs in the resource group and domain join them if they are already not...
+
+Install-VMDomainJoin. -resourceGroupName $ResourceGroupName -DomainCredential $domainUserCredential -Domain $domain
+
