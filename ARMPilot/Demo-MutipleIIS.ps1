f8e9eba3abad8c1fada9400258564c5c2e5fb534
Tracy Worthington <trworth@microsoft.com>
2016-03-14 16:54:51 -0700

    updates

--- ARMPilot/Demo-MutipleIIS.ps1
+++ ARMPilot/Demo-MutipleIIS.ps1
@@ -1,40 +1,48 @@
 ﻿
 # Scenario - Build 1 IIS server by namepart
-import-module cloudms
+#import-module cloudms
+
+# Image IIS
+
+        $params = @{
+                   "TemplateFile"="C:\Users\trworth\Source\Repos\SI-HDC-CPT-ARM\ARMPilot\templateIIS.json"; 
+                   "TemplateParameterFile"="C:\Users\trworth\Source\Repos\SI-HDC-CPT-ARM\ARMPilot\templateIISParams.json"; 
+                   "SubscriptionId"="e4a74065-cc6c-4f56-b451-f07a3fde61de"; 
+                   "ResourceGroupLocation"="central us"; 
+                   "ResourceGroupName"="cptApp1";
+                   "Domain"="Redmond.corp.microsoft.com"
+                  }
 
 #Get domain credentials that need to be used for domain joining the VMs
 $username= Read-Host -Prompt "Domain UserName (domainname\alias)"
 $password =Read-Host -Prompt "Password for $username" -AsSecureString
 $domainUserCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
- 
-# Image IIS
 
-        $params = @{
-                   "TemplateFile"="templateIIS.json"; 
-                   "TemplateParameterFile"="templateIISParams.json"; 
-                   "SubscriptionId"="e4a74065-cc6c-4f56-b451-f07a3fde61de"; 
-                   "ResourceGroupLocation"="central us"; 
-                   "ResourceGroupName"="cptApp1";
-                   
-                  }
+ $TempParams = Import-Templates -templatefile $params.templatefile -TemplateParameterFile $Params.TemplateParameterFile -vm "MyArmTestVM"
+ $u=$([string] $TempParams.localAdminUserName)
+ $p= ConvertTo-SecureString $([string] $TempParams.localAdminPassword) -asplaintext -force
+ $params.Domain = $TempParams.domainName
 
+ $LocalUserCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $u,$p
+                 
 #Enter your name and specifications for the IIS server.
-     Invoke-ARM -TemplateFile $params.TemplateFile `
-                -TemplateParameterFile $params.TemplateParameterFile `
-                -SubscriptionId $params.SubscriptionId `
-                -ResourceGroupLocation $params.ResourceGroupLocation `
-                -ResourceGroupName $params.ResourceGroupName 
+$serversBuilt=Invoke-ARM -TemplateFile $params.TemplateFile `
+                        -TemplateParameterFile $params.TemplateParameterFile `
+                        -SubscriptionId $params.SubscriptionId `
+                        -ResourceGroupLocation $params.ResourceGroupLocation `
+                        -ResourceGroupName $params.ResourceGroupName `
+                        -Vm "MyArmTestVM" `
+                        -creds $domainUserCredential 
 
-    Install-VMDomainJoin -resourceGroupName $params.ResourceGroupName  `
-                         -DomainCredential $domainUserCredential `
-                         -Domain $domain
-     
-# Build IIS
+Install-VMDomainJoin -Servers $serversBuilt `
+                        -SubscriptionId $params.SubscriptionId `
+                        -resourceGroupName $params.ResourceGroupName  `
+                        -DomainCredential $domainUserCredential `
+                        -LocalCredential $localUserCredential `
+                        -Domain $params.domain
 
-$mof=InstallIIS_DSC -outputPath $PSScriptRoot\InstallIIS_DSC `
-                    -MachineName ArmPilot-IIS-0 
-
-    Get-ChildItem -Path $mof.Directoryname | ConvertTo-MrMOFv4 -Verbose
-
-    Start-DscConfiguration $PSScriptRoot\InstallIIS_DSC -ComputerName ArmPilot-IIS-0   -Verbose -Wait
-
+Install-AdditionalAdmins -Servers $serversBuilt `
+                         -SubscriptionId $params.SubscriptionId `
+                         -resourceGroupName $params.ResourceGroupName `
+                         -creds $domainUserCredential `
+                         -AdditionalAdminList $TempParams.additionalAdmins
--- ARMPilot/Demo-MutipleSQL.ps1
+++ ARMPilot/Demo-MutipleSQL.ps1
@@ -4,34 +4,46 @@
 
 import-module cloudms
 
+
+    $params = @{
+                "TemplateFile"="C:\Users\trworth\Source\Repos\SI-HDC-CPT-ARM\ARMPilot\templateSQL.json"; 
+                "TemplateParameterFile"="C:\Users\trworth\Source\Repos\SI-HDC-CPT-ARM\ARMPilot\templateSQLParams.json"; 
+                "SubscriptionId"="e4a74065-cc6c-4f56-b451-f07a3fde61de"; 
+                "ResourceGroupLocation"="central us"; 
+                "ResourceGroupName"="cptApp1";
+                "Domain"="Redmond.corp.microsoft.com" 
+                }
+
 #Get domain credentials that need to be used for domain joining the VMs
 $username= Read-Host -Prompt "Domain UserName (domainname\alias)"
 $password =Read-Host -Prompt "Password for $username" -AsSecureString
 $domainUserCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
 
-    $params = @{
-                "TemplateFile"="templateSQL.json"; 
-                "TemplateParameterFile"="templateSQLParams.json"; 
-                "SubscriptionId"="e4a74065-cc6c-4f56-b451-f07a3fde61de"; 
-                "ResourceGroupLocation"="central us"; 
-                "ResourceGroupName"="cptApp1";
-                   
-                }
+ $TempParams = Import-Templates -templatefile $params.templatefile -TemplateParameterFile $Params.TemplateParameterFile -vm "MyArmTestVM"
+ $u=$([string] $TempParams.localAdminUserName)
+ $p= ConvertTo-SecureString $([string] $TempParams.localAdminPassword) -asplaintext -force
+ $params.Domain = $TempParams.domainName
 
-     Invoke-ARM -TemplateFile $params.TemplateFile `
-                -TemplateParameterFile $params.TemplateParameterFile `
-                -SubscriptionId $params.SubscriptionId `
-                -ResourceGroupLocation $params.ResourceGroupLocation `
-                -ResourceGroupName $params.ResourceGroupName 
+ $LocalUserCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $u,$p
+                 
+#Enter your name and specifications for the IIS server.
+$serversBuilt=Invoke-ARM -TemplateFile $params.TemplateFile `
+                        -TemplateParameterFile $params.TemplateParameterFile `
+                        -SubscriptionId $params.SubscriptionId `
+                        -ResourceGroupLocation $params.ResourceGroupLocation `
+                        -ResourceGroupName $params.ResourceGroupName `
+                        -Vm "MyArmTestVM" `
+                        -creds $domainUserCredential 
 
-# Build SQL
+Install-VMDomainJoin -Servers $serversBuilt `
+                        -SubscriptionId $params.SubscriptionId `
+                        -resourceGroupName $params.ResourceGroupName  `
+                        -DomainCredential $domainUserCredential `
+                        -LocalCredential $localUserCredential `
+                        -Domain $params.domain
 
-Setup-SQL -Servers @{"Servername"="ArmPilot-SQL-0"} `
-          -outputPath $PSScriptRoot\InstallSQL_DSC `
-          -ResourceGroupName $params.ResourceGroupName `
-          -DomainCredential  $domainUserCredential
-
-$mof=InstallSQL_DSC -outputPath $PSScriptRoot\InstallSQL_DSC -MachineName ArmPilot-SQL-0
-
-Get-ChildItem -Path $mof.Directoryname | ConvertTo-MrMOFv4 -Verbose
-Start-DscConfiguration $PSScriptRoot\InstallSQL_DSC -ComputerName ArmPilot-SQL-0   -Verbose -Wait
+Install-AdditionalAdmins -Servers $serversBuilt `
+                         -SubscriptionId $params.SubscriptionId `
+                         -resourceGroupName $params.ResourceGroupName `
+                         -creds $domainUserCredential `
+                         -AdditionalAdminList $TempParams.additionalAdmins
