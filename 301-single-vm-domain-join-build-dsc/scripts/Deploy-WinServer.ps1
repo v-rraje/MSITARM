1b46d0987a85d9007f8808cb98bcca9318480b38
Tracy Worthington <trworth@microsoft.com>
2016-05-20 15:30:49 -0700

    updated

--- 301-single-vm-domain-join-build-dsc/scripts/Deploy-WinServer.ps1
+++ 301-single-vm-domain-join-build-dsc/scripts/Deploy-WinServer.ps1
@@ -7,11 +7,14 @@ Configuration DeployWinServer
 
   Node ($MachineName)
   {
-	   
-   
-   return $true
-    
-	    
+	
+        cd\
+        if($(test-path -path c:\temp) -eq $false){
+            md Temp
+        }
+        $sw = New-Object System.IO.StreamWriter(“C:\Temp\Wininstall.log”)
+        $sw.WriteLine("$(Get-Date -Format g) Completed.") 
+	    $sw.Close()
   }
 
 }
