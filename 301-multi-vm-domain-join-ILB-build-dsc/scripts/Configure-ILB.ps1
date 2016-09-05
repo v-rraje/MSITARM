4513e334dec3df908220098cb92e0685dec1863a
Tracy Worthington <trworth@microsoft.com>
2016-09-05 05:13:05 -0700

    v1 release

--- 301-multi-vm-domain-join-ILB-build-dsc/scripts/Configure-ILB.ps1
+++ 301-multi-vm-domain-join-ILB-build-dsc/scripts/Configure-ILB.ps1
@@ -1,49 +1,33 @@
 ﻿# Name: ConfigureSQLserver
 #
-[CmdletBinding()]
 param
     (
-[parameter(Mandatory=$true, Position=0)]
 [string] $AOAGListenerName,
-
-[parameter(Mandatory=$true, Position=1)]
 [string] $AOAGName,
-
-[parameter(Mandatory=$true, Position=2)]
 [string] $ServernamePart,
-
-[parameter(Mandatory=$true, Position=3)]
 [string] $InstanceCount,
-
-[parameter(Mandatory=$true, Position=4)]
 [string] $FailoverClusterName,
-
-[parameter(Mandatory=$true, Position=5)]
 [string] $SubscriptionId,
-
-[Parameter(Mandatory=$true, Position=6)]
 [string] $uri,
-
-[Parameter(Mandatory=$true, Position=7)]
 [string] $SecretKey
-        
 )
 {
 
-$response = $null
+    $response = $null
 
-$headers = @{"From"="user@contoso.com";"Date"="05/28/2015 15:47:00"}
+    $headers = @{"From"="user@contoso.com";"Date"="$($(get-date).ToShortDateString())"}
 
-(1..$InstanceCount) | %{ if($_ -ne $instanceCount) { $nodes += "$servernamepart$_,"} else {$nodes += "$servernamepart$_"} }
+    (1..$InstanceCount) | %{ if($_ -ne $instanceCount) { $nodes += "$servernamepart$_,"} else {$nodes += "$servernamepart$_"} }
 
-$Params  = @(
-            @{ AOAGListenerName=$AOAGListenerName;AOAGName=$AOAGName;Nodes=$Nodes;FailoverClusterName=$FailoverClusterName;SubscriptionId=$SubscriptionId }
-            )
+    $Params  = @(
+                @{ AOAGListenerName=$AOAGListenerName;AOAGName=$AOAGName;Nodes=$Nodes;FailoverClusterName=$FailoverClusterName;SubscriptionId=$SubscriptionId }
+                )
 
-$body = ConvertTo-Json -InputObject $params
+    $body = ConvertTo-Json -InputObject $params
 
-$response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body 
-$response
+    $startRunbook = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
+    $jobID = $startRunbook.JobIds[0]
 
+    write-host $jobID
 
 }
