9d97abbcae4c198a5110844627eb6bced19c9be2
Tracy Worthington <trworth@microsoft.com>
2016-09-06 09:56:14 -0700

    V1 Release

--- 301-multi-vm-domain-join-ILB-build-dsc/scripts/Configure-ILB.ps1
+++ 301-multi-vm-domain-join-ILB-build-dsc/scripts/Configure-ILB.ps1
@@ -17,6 +17,7 @@ param
 [string] $SecretAcct
 
 )
+try {
 
     Import-Module cloudmsaad
 
@@ -35,31 +36,39 @@ param
     $startRunbook = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
     $jobID = $startRunbook.JobIds[0]
 
-    if($jobID) {
+        if($jobID) {
 
-        write-host $jobID
+            write-host $jobID
 
-        $jobstatusURL = "https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Automation/automationAccounts/{2}/jobs/{3}?api-version=2015-10-31" -f $SecretSubId,$SecretRg,$SecretAcct,$jobID
+            $jobstatusURL = "https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Automation/automationAccounts/{2}/jobs/{3}?api-version=2015-10-31" -f $SecretSubId,$SecretRg,$SecretAcct,$jobID
 
-        $i=0
+            $i=0
     
-        #check status of runbook
-        do{
-            $i++
-            $getRunbookStatus = Invoke-AzureRestGetAPI -Uri $jobstatusURL -clientId $SecretClientId -key $Secreturikey -tenantId $SecretTenantId
+            #check status of runbook
+            do{
+                $i++
+                $getRunbookStatus = Invoke-AzureRestGetAPI -Uri $jobstatusURL -clientId $SecretClientId -key $SecretKey -tenantId $SecretTenantId
         
-            $runbookStatus = $getRunbookStatus.properties.status
+                $runbookStatus = $getRunbookStatus.properties.status
             
-            if($runbookStatus) {
-                write-output "$(Get-Date) JobID: $jobID Status: $runbookStatus"
-                sleep -Seconds 5
-                if ($i -ge 100)
-                {
-                    Write-Output "Exceeded timeout, stopping status check of runbook"
-                    $runbookStatus = "Completed"
-                }
-            }else {write-host "job failed..."; break;}
-        }
-        until($runbookStatus -eq "Completed")
+                if($runbookStatus) {
+                    write-output "$(Get-Date) JobID: $jobID Status: $runbookStatus"
+                    sleep -Seconds 5
+                    if ($i -ge 100)
+                    {
+                        Write-Output "Exceeded timeout, stopping status check of runbook"
+                        $runbookStatus = "Completed"
+                    }
+                }else {write-host "job failed..."; break;}
+            }
+            until($runbookStatus -eq "Completed")
 
+        }
+
+    } catch { 
+            [string]$errorMessage = $_.Exception.Message
+            if([string]::IsNullOrEmpty($errorMessage) -ne $true) {
+                Write-EventLog -LogName Application -source AzureArmTemplates -eventID 3001 -entrytype Error -message "ConfigureDataPath: $errorMessage"
+            }
+            throw new Exception($errorMessage)
     }
