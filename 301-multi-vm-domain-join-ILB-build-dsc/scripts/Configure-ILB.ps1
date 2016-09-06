60437ea18744410f2e4b253e332b591e8be0c9a5
Tracy Worthington <trworth@microsoft.com>
2016-09-06 12:37:38 -0700

    V1 Release

--- 301-multi-vm-domain-join-ILB-build-dsc/scripts/Configure-ILB.ps1
+++ 301-multi-vm-domain-join-ILB-build-dsc/scripts/Configure-ILB.ps1
@@ -70,5 +70,5 @@ try {
             if([string]::IsNullOrEmpty($errorMessage) -ne $true) {
                 Write-EventLog -LogName Application -source AzureArmTemplates -eventID 3001 -entrytype Error -message "ConfigureDataPath: $errorMessage"
             }
-            throw $errorMessageSEALS01-CORP-EUS2-VNET-1
+            throw $errorMessage
     }
