49cc1f9195e9861764375e2e83aa0caa958d0c31
Tracy Worthington <trworth@microsoft.com>
2016-09-06 17:49:16 -0700

    V1 Release

--- 301-multi-vm-domain-join-ILB-build-dsc/scripts/Configure-ILB.ps1
+++ 301-multi-vm-domain-join-ILB-build-dsc/scripts/Configure-ILB.ps1
@@ -17,58 +17,78 @@ param
 [string] $SecretAcct
 
 )
-try {
 
-    Import-Module cloudmsaad
+ (1..$InstanceCount) | %{ if($_ -ne $instanceCount) { $nodes += "$servernamepart$_,"} else {$nodes += "$servernamepart$_"} }
+     
+    $i = 0
+    $serversOnline=$false
 
-    $response = $null
-    $uri = "https://s1events.azure-automation.net/webhooks?token={0}" -f $Secreturikey
-    $headers = @{"From"="user@contoso.com";"Date"="$($(get-date).ToShortDateString())"}
-
-    (1..$InstanceCount) | %{ if($_ -ne $instanceCount) { $nodes += "$servernamepart$_,"} else {$nodes += "$servernamepart$_"} }
-
-    $Params  = @(
-                @{ AOAGListenerName=$AOAGListenerName;AOAGName=$AOAGName;Nodes=$Nodes;FailoverClusterName=$FailoverClusterName;SubscriptionId=$SubscriptionId }
-                )
-
-    $body = ConvertTo-Json -InputObject $params
-    
-    $startRunbook = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
-    $jobID = $startRunbook.JobIds[0]
-
-        if($jobID) {
-
-            write-host $jobID
-
-            $jobstatusURL = "https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Automation/automationAccounts/{2}/jobs/{3}?api-version=2015-10-31" -f $SecretSubId,$SecretRg,$SecretAcct,$jobID
-
-            $i=0
-    
-            #check status of runbook
-            do{
-                $i++
-                $getRunbookStatus = Invoke-AzureRestGetAPI -Uri $jobstatusURL -clientId $SecretClientId -key $SecretKey -tenantId $SecretTenantId
+    do {
+            $i++
+            $Online=$true
+                foreach($ComputerName in $Nodes.split(",")) {
         
-                $runbookStatus = $getRunbookStatus.properties.status
+                    $test1 = test-connection $ComputerName -Count 1 -Quiet
+                    $Result = Invoke-Command -ComputerName $ComputerName -ScriptBlock {Get-Service | Where-Object {($_.Name -eq "WinRM") -and ($_.Status -eq "Running")}} -ErrorAction Stop 
+                    $test2 = !$Result?
+
+                    if(!$test1 -or !$test2) {$Online=$false}
+                }
+            $serversOnline = $online
+            if(!$Online) {sleep -Seconds 300}
+
+            #1 sleep 300 seconds, 12 sleeps is one hour, 48 is 4 hrs,  300 sleeps is 25 hrs
+            if($i -gt 300) {throw "Servers $nodes are not resolving online" }
+
+    } until($serversOnline)
+
+
+    if($serversOnline) {
+
+        Import-Module cloudmsaad
+
+        $response = $null
+        $uri = "https://s1events.azure-automation.net/webhooks?token={0}" -f $Secreturikey
+        $headers = @{"From"="user@contoso.com";"Date"="$($(get-date).ToShortDateString())"}
+
+       
+
+        $Params  = @(
+                    @{ AOAGListenerName=$AOAGListenerName;AOAGName=$AOAGName;Nodes=$Nodes;FailoverClusterName=$FailoverClusterName;SubscriptionId=$SubscriptionId }
+                    )
+
+        $body = ConvertTo-Json -InputObject $params
+    
+        $startRunbook = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
+        $jobID = $startRunbook.JobIds[0]
+
+            if($jobID) {
+
+                write-host $jobID
+
+                $jobstatusURL = "https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Automation/automationAccounts/{2}/jobs/{3}?api-version=2015-10-31" -f $SecretSubId,$SecretRg,$SecretAcct,$jobID
+
+                $i=0
+    
+                #check status of runbook
+                do{
+                    $i++
+                    $getRunbookStatus = Invoke-AzureRestGetAPI -Uri $jobstatusURL -clientId $SecretClientId -key $SecretKey -tenantId $SecretTenantId
+        
+                    $runbookStatus = $getRunbookStatus.properties.status
             
-                if($runbookStatus) {
-                    write-output "$(Get-Date) JobID: $jobID Status: $runbookStatus"
-                    sleep -Seconds 5
-                    if ($i -ge 100)
-                    {
-                        Write-Output "Exceeded timeout, stopping status check of runbook"
-                        $runbookStatus = "Completed"
-                    }
-                }else {write-host "job failed..."; break;}
+                    if($runbookStatus) {
+                        write-output "$(Get-Date) JobID: $jobID Status: $runbookStatus"
+                        sleep -Seconds 5
+                        if ($i -ge 100)
+                        {
+                            Write-Output "Exceeded timeout, stopping status check of runbook"
+                            $runbookStatus = "Failed"
+                        }
+                    }else {write-host "job failed..."; $runbookStatus = "Failed"; break;}
+                }
+                until($runbookStatus -eq "Completed" -or $runbookStatus -eq "Failed")
+
             }
-            until($runbookStatus -eq "Completed")
 
         }
-
-    } catch { 
-            [string]$errorMessage = $_.Exception.Message
-            if([string]::IsNullOrEmpty($errorMessage) -ne $true) {
-                Write-EventLog -LogName Application -source AzureArmTemplates -eventID 3001 -entrytype Error -message "ConfigureDataPath: $errorMessage"
-            }
-            throw $errorMessage
-    }
