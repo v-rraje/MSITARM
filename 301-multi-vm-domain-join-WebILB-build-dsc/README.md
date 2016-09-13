7c8e1788d5bf583bec6ef886822e14ecef806706
Tracy Worthington <trworth@microsoft.com>
2016-09-13 11:51:46 -0700

    V1 Release

---
+++ 301-multi-vm-domain-join-WebILB-build-dsc/README.md
@@ -0,0 +1,11 @@
+Deployment of multiple VMs that includes ILB and availability group set-up as well.  Features include:
+•	Creates Domain Joined VMs
+•	Adds them to availability set
+•	Create Internal Load Balancer
+•	Configures ILB Rules
+•	Configures Probe
+•	Creates Probe Test
+•	Configures Backed Pool for ILB
+•	Adds Availability Set to backend pool
+
+This is a community submission from the Mercury team.  Please reach out to merceng@microsoft.com for questions\consult.
---
+++ 301-multi-vm-domain-join-WebILB-build-dsc/scripts/Configure-ILB.ps1
@@ -0,0 +1,58 @@
+﻿# Name: ConfigureSQLserver
+#
+param
+    (
+[string] $AOAGListenerName,
+[string] $AOAGName,
+[string] $ServernamePart,
+[string] $InstanceCount,
+[string] $Domain,
+[string] $FailoverClusterName,
+[string] $SubscriptionId,
+[string] $SecretClientId,
+[string] $Secreturikey,
+[string] $SecretKey,
+[string] $SecretSubId,
+[string] $SecretTenantId,
+[string] $SecretRg,
+[string] $SecretAcct
+
+)
+try {
+
+ $nodes=""
+
+ (1..$InstanceCount) | %{ if($_ -ne $instanceCount) { $nodes += "$servernamepart$_,"} else {$nodes += "$servernamepart$_"} }
+   
+ 
+        Import-Module cloudmsaad
+
+        $response = $null
+        $uri = "https://s1events.azure-automation.net/webhooks?token={0}" -f $Secreturikey
+        $headers = @{"From"="user@contoso.com";"Date"="$($(get-date).ToShortDateString())"}
+               
+
+        $Params  = @(
+                    @{ AOAGListenerName=$AOAGListenerName;AOAGName=$AOAGName;Nodes=$Nodes;FailoverClusterName=$FailoverClusterName;SubscriptionId=$SubscriptionId }
+                    )
+
+        $body = ConvertTo-Json -InputObject $params
+    
+        $startRunbook = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
+        $jobID = $startRunbook.JobIds[0]
+
+            if($jobID) {
+                            
+               $jobstatusURL = "see Dashboard. 'http://co1cptdevweb01:4433/?searchText={0}&f_mtype=SQLAO-Configuration&f_dateType=all'  " -f $AOAGListenerName
+                
+               write-host $jobstatusURL
+
+            }
+
+        } catch {
+         [string]$errorMessage = $_.Exception.Message
+         if([string]::IsNullOrEmpty($errorMessage) -ne $true) {
+            Write-EventLog -LogName Application -source AzureArmTemplates -eventID 3001 -entrytype Error -message "ConfigureDataPath: $errorMessage"
+         }
+            throw $errorMessage
+        }
