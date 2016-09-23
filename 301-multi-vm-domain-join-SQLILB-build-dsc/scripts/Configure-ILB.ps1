b34164fe9e199b1098757b458d22e7eb246d3eba
Tracy Worthington <trworth@microsoft.com>
2016-09-23 07:50:48 -0700

    V2 Release

---
+++ 301-multi-vm-domain-join-SQLILB-build-dsc/scripts/Configure-ILB.ps1
@@ -0,0 +1,60 @@
+﻿# Name: ConfigureSQLserver
+#
+param
+    (
+[string] $AOAGListenerName,
+[string] $AOAGName,
+[string] $LoadBalancerName,
+[string] $ServernamePart,
+[string] $InstanceCount,
+[string] $Domain,
+[string] $FailoverClusterName,
+[string] $SubscriptionId,
+[string] $Secreturikey,
+[string] $SecretKey,
+[string] $SecretSubId,
+[string] $SecretRg,
+[string] $SecretAcct,
+[string] $dashboardURL
+
+)
+try {
+
+ $nodes=""
+
+ (1..$InstanceCount) | %{ if($_ -ne $instanceCount) { $nodes += "$servernamepart$_,"} else {$nodes += "$servernamepart$_"} }
+   
+ 
+        Import-Module cloudmsaad
+
+        $response = $null
+        $uri = "https://s1events.azure-automation.net/webhooks?token={0}" -f $Secreturikey
+        $headers = @{"From"="user@contoso.com";"Date"="$($(get-date).ToShortDateString())"}
+               
+
+        $Params  = @(
+                    @{ AOAGListenerName=$AOAGListenerName;ILBName=$LoadBalancerName;AOAGName=$AOAGName;Nodes=$Nodes;FailoverClusterName=$FailoverClusterName;SubscriptionId=$SubscriptionId }
+                    )
+
+        $body = ConvertTo-Json -InputObject $params
+    
+        $startRunbook = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
+        $jobID = $startRunbook.JobIds[0]
+
+            if($jobID) {
+                            
+               $jobstatusURL = "see Dashboard. Copy/paste this link-> 'http://{0}/?searchText={1}&f_mtype=SQLAO-Configuration&f_dateType=all'  " -f $dashboardURL,$AOAGListenerName
+                
+               write-host $jobstatusURL
+
+            }
+
+        } catch {
+
+             [string]$errorMessage = $_.Exception.Message
+             if([string]::IsNullOrEmpty($errorMessage) -ne $true) {
+                Write-EventLog -LogName Application -source AzureArmTemplates -eventID 3001 -entrytype Error -message "ConfigureDataPath: $errorMessage"
+             }
+
+            throw $errorMessage
+        }
